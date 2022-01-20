4b27e75130a4e8cd8aa2efda411c7e2afd0029e1
Wayne Dovey <wdovey@gmail.com>
2022-01-20 13:58:52 +1100

    moving to dev env

--- database-init/db-init.sh
+++ database-init/db-init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+oc project bank-infra
+
 oc process postgresql-persistent -p DATABASE_SERVICE_NAME=creditdb -p POSTGRESQL_USER=postgres -p POSTGRESQL_PASSWORD=postgres -p POSTGRESQL_DATABASE=example -n openshift |  oc create -f -
 
 oc apply -f job.yaml
+oc apply -f serviceexport.yaml
